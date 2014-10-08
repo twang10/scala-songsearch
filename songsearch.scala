bee5e26231438eeb5cd491519915c04f113026cc
Tom <twang10@gmail.com>
2014-10-07 23:58:30 -0400

    class changes

--- songsearch.scala
+++ songsearch.scala
@@ -1,16 +1,14 @@
-import scala.collection.mutable.Map
-
-class Song(t: String, a: String) {
+class Song(t: String, a: String, l: Array[String]) {
 	val title: String = t
 	val artist: String = a
-	var lyrics = Array[String]_
-	var word_count = Map.empty[String, Int]
+	val lyrics = l
+	var word_count:Map[String, Int] = Map()
 
 
 	def count(word: String):String = {
 		/* Called .get here to return an option */
 		word_count.get(word) match {
-			case None => word_count(word) = 1
+			case None => word_count += (word => 1)
 			case _ => word_count(word) = word_count(word) + 1
 		}
 		return word
